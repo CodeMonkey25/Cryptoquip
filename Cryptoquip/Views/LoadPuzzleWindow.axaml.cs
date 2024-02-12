dcf00a2dde5fc1f3ab9e1d20fc856c8b8c3427c1
Jason Sarley <jsarley@lcs.com>
2024-02-12 15:03:50 -0500

    adjusted word gaps and mainwindow size

--- Cryptoquip/Views/LoadPuzzleWindow.axaml.cs
+++ Cryptoquip/Views/LoadPuzzleWindow.axaml.cs
@@ -1,14 +1,15 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
-using Avalonia.Markup.Xaml;
 
 namespace Cryptoquip.Views;
 
 public partial class LoadPuzzleWindow : Window
 {
     public static readonly DirectProperty<LoadPuzzleWindow, string> PuzzleTextProperty = AvaloniaProperty.RegisterDirect<LoadPuzzleWindow, string>(nameof(PuzzleText), o => o.PuzzleText, (o, v) => o.PuzzleText = v);
-    private string _puzzleText = "VE TEW ZWSSEZX PAGP GHT ESAPAGKDEKENFZP'Z SBXQXBBXV UBXGCQGZP MXBXGK DFNAP UX MEBHXG QKGCXZ? <HINT>: T=Y";
+    // private string _puzzleText = "VE TEW ZWSSEZX PAGP GHT ESAPAGKDEKENFZP'Z SBXQXBBXV UBXGCQGZP MXBXGK DFNAP UX MEBHXG QKGCXZ? <HINT>: T=Y";
+    // private string _puzzleText = "CIY JVO J TBNQL IG ZBO YDNKOVMO, DI QOMM ZBJD ZBO AIID JDL ZBO MZJVM; CIY BJKO J VNEBZ ZI HO BOVO. JDL RBOZBOV IV DIZ NZ NM TQOJV ZI CIY, DI LIYHZ ZBO YDNKOVMO NM YDGIQLNDE JM NZ MBIYQL. - AJS OBVAJDD";
+    private string _puzzleText = "X KYP Y DSSW SF EYEEG'K BYVU LSEYG YFE YDD SB Y KHEEUF X BSHFE CGKUDB DSSWXFN YL RXC FSL YK CG EYEEG OHL YK YFSLRUZ CYF. CYGOU X'C NZSPXFN HQ Y DXLLDU. CYGOU XL PYK LRU QYXF XF RXK BYVU. OHL XL VYCU LS CU LRYL BSZ YDD LRU QDUYKHZU CSCCY YFE EYEEG BXFE XF HK VRXDEZUF, LRUZU YZU SLRUZ LXCUK XL CHKL OU RUYZLOZUYWXFN LS LRUC. <HINT>:EYEEG'K=DADDY'S";
     public string PuzzleText
     {
         get => _puzzleText;
