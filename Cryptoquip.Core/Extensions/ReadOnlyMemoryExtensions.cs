d2ae894e0b94e88d59028df3bcc72180faefc14b
Jason Sarley <jsarley@lcs.com>
2026-09-01 11:03:23 -0400

    updated ReadOnlyMemoryExtensions with new split overloads, added Any and All for generics, and refactored Prepend/Append

--- Cryptoquip.Core/Extensions/ReadOnlyMemoryExtensions.cs
+++ Cryptoquip.Core/Extensions/ReadOnlyMemoryExtensions.cs
@@ -2,36 +2,65 @@
 
 public static class ReadOnlyMemoryExtensions
 {
-    public static IEnumerable<ReadOnlyMemory<char>> Split(this ReadOnlyMemory<char> source, char splitChar)
+    public static IEnumerable<ReadOnlyMemory<char>> Split(this ReadOnlyMemory<char> source, char splitChar, StringSplitOptions options = StringSplitOptions.None)
     {
-        int start = 0;
-        for (int i = 0; i < source.Length; i++)
+        int separatorCount = source.Span.Count(splitChar);
+        Range[] ranges = new Range[separatorCount + 1];
+        int count = source.Span.Split(ranges, splitChar, options);
+        var result = new ReadOnlyMemory<char>[count];
+        for (int i = 0; i < count; i++)
         {
-            char c = source.Span[i];
-            if (c != splitChar) continue;
-            yield return source.Slice(start, i - start);
-            start = i + 1;
+            result[i] = source[ranges[i]];
         }
-		
-        if (start < source.Length)
-        {
-            yield return source.Slice(start);
-        }
+        return result;
     }
-	
+
+    public static IEnumerable<ReadOnlyMemory<char>> Split(this ReadOnlyMemory<char> source, ReadOnlySpan<char> separator, StringSplitOptions options = StringSplitOptions.None)
+    {
+        int separatorCount = source.Span.Count(separator);
+        Range[] ranges = new Range[separatorCount + 1];
+        int count = source.Span.Split(ranges, separator, options);
+        var result = new ReadOnlyMemory<char>[count];
+        for (int i = 0; i < count; i++)
+        {
+            result[i] = source[ranges[i]];
+        }
+        return result;
+    }
+
+    public static bool Any<T>(this ReadOnlyMemory<T> source, Func<T, bool> predicate)
+    {
+        foreach (T item in source.Span)
+        {
+            if (predicate(item))
+                return true;
+        }
+        return false;
+    }
+
     public static bool Any(this ReadOnlyMemory<char> source, Predicate<char> predicate)
     {
-        foreach(char c in source.Span)
+        foreach (char c in source.Span)
         {
             if (predicate(c))
                 return true;
         }
         return false;
     }
-	
+
+    public static bool All<T>(this ReadOnlyMemory<T> source, Func<T, bool> predicate)
+    {
+        foreach (T item in source.Span)
+        {
+            if (!predicate(item))
+                return false;
+        }
+        return true;
+    }
+
     public static bool All(this ReadOnlyMemory<char> source, Predicate<char> predicate)
     {
-        foreach(char c in source.Span)
+        foreach (char c in source.Span)
         {
             if (!predicate(c))
                 return false;
@@ -39,30 +68,17 @@ public static class ReadOnlyMemoryExtensions
         return true;
     }
 
-    public static IEnumerable<ReadOnlyMemory<T>> Prepend<T>(this IEnumerable<ReadOnlyMemory<T>> source, ReadOnlyMemory<T> value)
-    {
-        yield return value;
-        foreach (ReadOnlyMemory<T> item in source)
-        {
-            yield return item;
-        }
-    }
-    
-    public static IEnumerable<ReadOnlyMemory<T>> Append<T>(this IEnumerable<ReadOnlyMemory<T>> source, ReadOnlyMemory<T> value)
-    {
-        foreach (ReadOnlyMemory<T> item in source)
-        {
-            yield return item;
-        }
-        yield return value;
-    }
-    
+    public static IEnumerable<ReadOnlyMemory<T>> Prepend<T>(this IEnumerable<ReadOnlyMemory<T>> source, ReadOnlyMemory<T> value) =>
+        Enumerable.Prepend(source, value);
+
+    public static IEnumerable<ReadOnlyMemory<T>> Append<T>(this IEnumerable<ReadOnlyMemory<T>> source, ReadOnlyMemory<T> value) =>
+        Enumerable.Append(source, value);
+
     public static IEnumerable<TResult> Select<T, TResult>(this ReadOnlyMemory<T> source, Func<T, TResult> selector)
     {
-        for (int i = 0; i < source.Span.Length; i++)
+        for (int i = 0; i < source.Length; i++)
         {
-            T item = source.Span[i];
-            yield return selector(item);
+            yield return selector(source.Span[i]);
         }
     }
 }
