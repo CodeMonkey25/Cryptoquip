b5d3d872f44300aa5f07b4956d9a17c8c92aa418
Jason Sarley <jsarley@lcs.com>
2026-09-22 09:29:04 -0400

    refactored MatchRequirements.Build to use a static method

--- Cryptoquip.Core/Models/MatchRequirements.cs
+++ Cryptoquip.Core/Models/MatchRequirements.cs
@@ -6,7 +6,7 @@ public abstract class MatchRequirements
     
     public static MatchRequirements Build(string text, List<string> matches)
     {
-        MatchRequirements requirements = new MatchRequirementsBitmask();
+        MatchRequirements requirements = MatchRequirements.Build();
         requirements.Rebuild(text, matches);
         return requirements;
     }
